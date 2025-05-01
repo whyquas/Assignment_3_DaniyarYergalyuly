ee49905a394d8cbf8dfb51fd0dfc20d604f30e30
whyquas <qusaratonkachi@gmail.com>
2025-05-01 23:36:13 +0500

    MyTestingClass

---
+++ src/MyTestingClass.java
@@ -0,0 +1,27 @@
+import java.util.Objects;
+
+public class MyTestingClass {
+    private int id;
+
+    public MyTestingClass(int id) {
+        this.id = id;
+    }
+
+    @Override
+    public int hashCode() {
+        return (id * 31 + 17) ^ (id >>> 3);
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (!(obj instanceof MyTestingClass)) return false;
+        MyTestingClass other = (MyTestingClass) obj;
+        return id == other.id;
+    }
+
+    @Override
+    public String toString() {
+        return "TestClass{" + id + '}';
+    }
+}
